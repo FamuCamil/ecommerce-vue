9eb13606e4cc1fecf8a6a6087b8ea32a03cb6dca
Fanny <42516302+FamuCamil@users.noreply.github.com>
2025-01-26 14:11:55 +0100

    Inicio del proyecto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <button class="ui primary button">Save</button>
+    <button class="ui button">Discard</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import BasicLayout from "@/layouts/BasicLayout.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    BasicLayout,
+  },
+};
 </script>
